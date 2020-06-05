8d6ce49651d1e2ee29ef9e51247a51132bbdd385
audreyyyA <47739698+audreyyyA@users.noreply.github.com>
2020-06-05 12:41:43 +0200

    modif des erreurs

--- app/src/main/java/com/dut/kidoi/LoginActivity.java
+++ app/src/main/java/com/dut/kidoi/LoginActivity.java
@@ -1,73 +1,52 @@
 package com.dut.kidoi;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+/*import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;*/
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-import com.dut.kidoi.models.Users;
+import com.dut.kidoi.models.User;
 
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
-    private ArrayList<Users> myDataSet = new ArrayList<Users>();
-=======
-public class LoginActivity extends AppCompatActivity {
 
-    private TextView link_connect;
->>>>>>> 3c9c801e540d5f81b4b221a96ffff3eee41b2b00
+
+    private ArrayList<User> myDataSet = new ArrayList<User>();
 
     private TextView link_connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-<<<<<<< HEAD
 
         link_connect = findViewById(R.id.tv_inscription);
 
-        /*
-            Passer de la page de connexion à celle d'inscription
-         */
-        link_connect.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this, SignUp.class));
-            }
 
-        });
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        /*recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        Users u = new Users();
+        User u = new User();
 
-        myDataSet.add(u);
+        myDataSet.add(u); */
 
-
-
-=======
-
-        link_connect = findViewById(R.id.tv_inscription);
 
         /*
             Passer de la page de connexion à celle d'inscription
          */
-        link_connect.setOnClickListener(new View.OnClickListener(){
+
+        link_connect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, SignUp.class));
             }
 
         });
->>>>>>> 3c9c801e540d5f81b4b221a96ffff3eee41b2b00
     }
 }
