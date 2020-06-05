3c9c801e540d5f81b4b221a96ffff3eee41b2b00
audreyyyA <47739698+audreyyyA@users.noreply.github.com>
2020-06-05 11:53:28 +0200

    pb de contrainte signUp

--- app/src/main/java/com/dut/kidoi/LoginActivity.java
+++ app/src/main/java/com/dut/kidoi/LoginActivity.java
@@ -2,13 +2,31 @@ package com.dut.kidoi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-        public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView link_connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        link_connect = findViewById(R.id.tv_inscription);
+
+        /*
+            Passer de la page de connexion à celle d'inscription
+         */
+        link_connect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this, SignUp.class));
+            }
+
+        });
     }
 }
