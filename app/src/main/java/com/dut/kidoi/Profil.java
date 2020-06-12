70778179da19aae1ceeb4daa2357d6d724da2187
audreyyyA <47739698+audreyyyA@users.noreply.github.com>
2020-06-12 11:05:56 +0200

    visu sign up done (manque color)

--- app/src/main/java/com/dut/kidoi/Profil.java
+++ app/src/main/java/com/dut/kidoi/Profil.java
@@ -23,6 +23,7 @@ public class Profil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
 
         /**
          * Menu fixe en bas
