fbfa34ca1faa6021159b8dfda30c893f94aa1bdb
ras-000212 <56430773+ras-000212@users.noreply.github.com>
2020-06-23 23:42:17 +0200

    jsp

--- app/src/main/java/com/dut/kidoi/repositories/FirebaseRepository.java
+++ app/src/main/java/com/dut/kidoi/repositories/FirebaseRepository.java
@@ -3,9 +3,15 @@ package com.dut.kidoi.repositories;
 import android.telecom.Call;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.dut.kidoi.models.User;
 import com.dut.kidoi.utils.Callback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -37,12 +43,15 @@ public class FirebaseRepository {
         user.put("username", username);
         user.put("userID", uID);
 
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(documentReference -> {
-                    Log.d("", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    if(cb != null)
-                        getUser(uID, cb);
+        db.collection("users").document(username)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("", "DocumentSnapshot added with ID: ");
+                        if (cb != null)
+                            getUser(uID, cb);
+                    }
                 })
                 .addOnFailureListener(e -> {
                     Log.w("", "Error adding document", e);
@@ -74,25 +83,30 @@ public class FirebaseRepository {
             }
         });
     }
+/*
+    public  void getUserLogin(String username, Callback<User> cb) {
 
-    public void getUserLogin(String username, Callback<User> cb) {
-        db.collection("users").whereEqualTo("username", username).get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                QuerySnapshot document = task.getResult();
+        db.collection("users").document("test")
+                .get()
+                .addOnSuccessListener(task -> {
+                        if (task.isSuccesful()) {
+                            QuerySnapshot document = task.getResult();
 
-                if (document.getDocuments().isEmpty()) {
-                    cb.call(null);
-                    return;
+                            if (document.getDocuments().isEmpty()) {
+                                cb.call(null);
+                                return;
+                            }
+
+                            cb.call(new User(
+                                    document.getDocuments().get(0).getString("username"),
+                                    document.getDocuments().get(0).getString("email"),
+                                    document.getDocuments().get(0).getString("userID")
+                            ));
+                        }
+                    });
                 }
 
-                cb.call(new User(
-                        document.getDocuments().get(0).getString("username"),
-                        document.getDocuments().get(0).getString("email"),
-                        document.getDocuments().get(0).getString("userID")
-                ));
-            }
-        });
-    }
+*/
 
     public void getUser(Callback<User> cb){
         getUser(FirebaseAuth.getInstance().getCurrentUser().getUid(), user -> {
@@ -108,4 +122,10 @@ public class FirebaseRepository {
     public void reset(){
         connectedUser = null;
     }
+
+    public void transaction(String credit,String debit,String montant,String motif){
+        //db.collection("users").whereEqualTo("username", credit).get().add
+    }
+
+
 }
